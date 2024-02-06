673ff6cd8ef19cab5c23563b853e919772ce5f8b
Hamid <Bouayadihamid@gmail.com>
2024-02-06 11:34:33 -0800

    Update

--- models/base_model.py
+++ models/base_model.py
@@ -5,8 +5,8 @@ import uuid
 class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.create_at = datetime.datetime.now()
-        self.update_at = datetime.datetime.now()
+        self.create_at = datetime.now()
+        self.update_at = datetime.now()
     def __str__(self):
         print(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
