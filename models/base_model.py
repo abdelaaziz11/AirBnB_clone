a988c903c643d496d04c96f4a4bbb22f1126e2e4
Hamid <Bouayadihamid@gmail.com>
2024-02-06 13:08:58 -0800

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
+from datetime import datetime
+from uuid import uuid4
+import models
 
-import datetime
-import uuid
 class BaseModel:
     def __init__(self):
-        self.id = str(uuid.uuid4())
+        self.id = str(uuid4())
         self.create_at = datetime.now()
         self.update_at = datetime.now()
+
+    def save(self):
+        self.update_at = datetime.now()
+    """def __str__(self):
+        return(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")"""
+    def to_dict(self):
+        dict_obj = self.__dict__.copy()
+        dict_obj['__class__'] = self.__class__.__name__
+        dict_obj['create_at'] = self.create_at.isoformat()
+        dict_obj['update_at'] = self.update_at.isoformat()
+        return dict_obj
     def __str__(self):
-        print(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
-
-#def save(self):
-
-#def to_dict(self):
-
+        return(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
