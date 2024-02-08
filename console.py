b864a7c56e48e018909a6c825f5eb855ecbb89a0
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-02-08 13:19:00 -0800

    console task

--- console.py
+++ console.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""console file"""
 
 import cmd
 
@@ -6,11 +7,16 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
+        """to exit the program"""
         return True
+
     def do_EOF(self, arg):
+        """to exit the program"""
         print("")
         return True
+
     def emptyline(self):
+        """do nothing"""
         pass
 
 if __name__ == '__main__':
