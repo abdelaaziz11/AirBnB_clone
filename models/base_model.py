dca384b53909698fa2d39e3681c0975d7adb25ec
Hamid <Bouayadihamid@gmail.com>
2024-02-08 15:47:45 -0800

    Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 from datetime import datetime
 from uuid import uuid4
 import models
+from models.__init__ import storage
 
 class BaseModel:
     def __init__(self, *arg, **kwargs):
@@ -13,8 +14,10 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
     """def __str__(self):
         return(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")"""
     def to_dict(self):
---
+++ models/engine/file_storage.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import json
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+    def all(self):
+        return self.__objects
+    def new(self, obj):
+        key = f"{type(obj).__name__}.{obj.id}"
+        self.__objects[key] = obj.to_dict()
+    def save(self):
+        with open(self.__file_path, 'w') as file:
+            json.dump(self.__objects, file)
+    def reload(self):
+        try:
+            with open(self.__file_path, 'r') as file:
+                self.__objects = json.load(file)
+        except FileNotFoundError:
+            pass
