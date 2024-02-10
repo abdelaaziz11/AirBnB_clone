467d62bb5040653e84ab438a6b72148af1b3b136
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-02-10 10:25:03 -0800

    changes

--- models/base_model.py
+++ models/base_model.py
@@ -3,24 +3,25 @@
 from datetime import datetime
 from uuid import uuid4
 import models
-from models.__init__ import storage
+#from models.__init__ import storage
 
 class BaseModel:
-    def __init__(self, *arg, **kwargs):
+    def init__(self):
+    """def __init__(self, *arg, **kwargs):
         if kwargs:
             for keys, values in kwargs.items():
                 if keys != '__class__':
                     setattr(self, keys, values)
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+        else:"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        #storage.new(self)
 
     def save(self):
         """updates the public instance attribute"""
         self.updated_at = datetime.now()
-        storage.save()
+        #storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
