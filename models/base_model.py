2f48c4ced90e1bf1c44794782acc41c797fa36c8
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-02-10 08:59:45 -0800

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 from datetime import datetime
 from uuid import uuid4
 import models
-from models.__init__ import storage
+"""from models.__init__ import storage"""
 
 class BaseModel:
     def __init__(self, *arg, **kwargs):
@@ -27,12 +27,12 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def save(self):
         """updates the public instance attribute"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
     """def __str__(self):
         return(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")"""
 
