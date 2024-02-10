bd780ae761bb4ab98abaa39683c215df614be4f3
Hamid <Bouayadihamid@gmail.com>
2024-02-10 11:56:04 -0800

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -2,25 +2,19 @@
 """The BaseModel Class"""
 from datetime import datetime
 from uuid import uuid4
-import models
-from models.__init__ import storage
+#import models
+#from models.__init__ import storage
 
 class BaseModel:
-    def __init__(self, *arg, **kwargs):
-        if kwargs:
-            for keys, values in kwargs.items():
-                if keys != '__class__':
-                    setattr(self, keys, values)
-        else:
+    def __init__(self):
             self.id = str(uuid4())
             self.created_at = datetime.today()
-            self.updated_at = datetime.today()
-            storage.new(self)
+            self.updated_at = self.created_at
 
     def save(self):
         """updates the public instance attribute"""
         self.updated_at = datetime.today()
-        storage.save()
+        #storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
