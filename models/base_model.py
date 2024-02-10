c9101e0685c23e69fd2598d7b94db0a4e26c0bda
Hamid <Bouayadihamid@gmail.com>
2024-02-10 13:03:18 -0800

    doc

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from uuid import uuid4
 import models
 
+
 class BaseModel:
     """BaseMode for AirBnB project."""
 
@@ -24,14 +25,12 @@ class BaseModel:
         else:
             models.storage.new(self)
     def save(self):
-        """updates the public instance attribute"""
-     
+        """updates the public instance attribute"""     
         self.updated_at = datetime.today()
         #storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
-        
         dict_obj = self.__dict__.copy()
         dict_obj['created_at'] = self.created_at.isoformat()
         dict_obj['updated_at'] = self.updated_at.isoformat()
@@ -39,7 +38,6 @@ class BaseModel:
         return dict_obj
     #def __str__(self):
         """print: [<class name>] (<self.id>) <self.__dict__>"""
-        
      #   return f"[{self.__class__.__name__}]({self.id}) {self.__dict__}"
     def __str__(self):
         """print: [<class name>] (<self.id>) <self.__dict__>"""
