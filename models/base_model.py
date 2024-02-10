fdcf745b835c247a7396e263c957bd602bbffa66
Hamid <Bouayadihamid@gmail.com>
2024-02-10 12:48:28 -0800

    all

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ import models
 
 class BaseModel:
     """BaseMode for AirBnB project."""
+
     def __init__(self, *args, **kwargs):
         """Create a new BaseModel.
         Args: *args is unused and **kwargs is for attributes."""
@@ -24,11 +25,13 @@ class BaseModel:
             models.storage.new(self)
     def save(self):
         """updates the public instance attribute"""
+     
         self.updated_at = datetime.today()
         #storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
+        
         dict_obj = self.__dict__.copy()
         dict_obj['created_at'] = self.created_at.isoformat()
         dict_obj['updated_at'] = self.updated_at.isoformat()
@@ -36,4 +39,5 @@ class BaseModel:
         return dict_obj
     def __str__(self):
         """print: [<class name>] (<self.id>) <self.__dict__>"""
+        
         return f"[{self.__class__.__name__}]({self.id}) {self.__dict__}"
