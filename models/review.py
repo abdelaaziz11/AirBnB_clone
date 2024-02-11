edc776818ff221b374b30e41e2e9dc1e02b669cf
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-02-10 18:38:36 -0800

    user

---
+++ models/review.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""The Review class."""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Review class """
+
+    place_id = ""
+    user_id = ""
+    text = ""
