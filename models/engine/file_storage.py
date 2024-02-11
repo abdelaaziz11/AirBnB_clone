ebbe0207f91479230f4428bbb432a8e01c38ec00
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-02-10 20:04:29 -0800

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,41 +1,38 @@
 #!/usr/bin/python3
 """ FileStorage that serializes instances to a JSON file and deserializes JSON
-file to instances:
 """
 
 import json
 import os
 from models.base_model import BaseModel
-#from models.amenity import Amenity
-#from models.city import City
-#from models.place import Place
-#from models.review import Review
-#from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 from models.user import User
 
 
 class FileStorage():
-    """ serializes instances to a JSON file and deserializes JSON"""
+    """ serializes instances to a JSON file 
+        and deserializes JSON
+    """
 
     __file_path = "file.json"  # path to the JSON file (ex: file.json)
     __objects = {}  # dictionary - store all objects by <class name>.id
 
     def all(self):
-        """  returns the dictionary __objects """
+        """returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
-        """ sets in __objects the obj with key <obj class name>.id """
-        '''get key of the form <obj class name>.id '''
+        """sets in __objects the obj with key"""
         key = obj.__class__.__name__ + "." + str(obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
-
-        ''' create empty dictionary'''
+        """serializes __objects to the JSON file"""
         json_object = {}
-        """ fill dictionary with elements __objects """
         for key in self.__objects:
             json_object[key] = self.__objects[key].to_dict()
 
@@ -43,7 +40,7 @@ class FileStorage():
             json.dump(json_object, f)
 
     def reload(self):
-        """Deserializes the JSON file to __objects if it exists"""
+        """JSON file to __objects if it exists"""
         if os.path.exists(type(self).__file_path) is True:
             return
             try:
