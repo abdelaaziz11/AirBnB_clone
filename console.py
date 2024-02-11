a5103c3b84089e668dd3f76b48d31a85c0669dd9
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-02-11 09:55:53 -0800

    changes

--- console.py
+++ console.py
@@ -5,7 +5,7 @@ import re
 import json
 import cmd
 from models.base_model import BaseModel
-from models import storage
+from models.__init__ import storage
 from models.user import User
 from models.state import State
 from models.city import City
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -23,7 +23,6 @@ class_dict = {
     "Review": Review,
     "State": State
 }
-# Filestorage == type(self)
 
 
 class FileStorage:
@@ -44,21 +43,15 @@ class FileStorage:
             return
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         type(self).__objects[key] = obj
-        # OR
-        # type(self).__objects[obj.id] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         new_dict = []
         for obj in type(self).__objects.values():
             new_dict.append(obj.to_dict())
-            # for key, obj in type(self).__objects.items():
-            #    new_dict[key] = obj.to_dict()
+
         with open(type(self).__file_path, "w", encoding='utf-8') as file:
             json.dump(new_dict, file)
-            # OR
-            # with open(type(self).__file_path, "w", encoding="utf-8") as file:
-            #   json.dump([obj.to_dict() for obj in self.all().values()], file)
 
     def reload(self):
         """Deserializes the JSON file to __objects if it exists"""
